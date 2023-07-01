2d557f3132e69bfa27301aa16a6a4f64a9e1f38a
Colette Moucheron <mcmoucheron@gmail.com>
2023-07-01 14:32:52 +0000

    Agregando buscador

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
 class Tweet < ApplicationRecord
+    include PgSearch::Model
+
+# Agregamos una dependencia como buscador determinando los 
+# parametros de la bd a consultar
+
+    pg_search_scope :search_full_text,
+against: {
+        username:'A',
+    }
 end
+

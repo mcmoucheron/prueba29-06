dafdae4f5f469b3fca491b26dd8ef52150dbae01
Colette Moucheron <mcmoucheron@gmail.com>
2023-07-12 15:29:35 +0000

    se agrega archivo render.yaml

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-#worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -41,4 +41,3 @@ preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-#art
